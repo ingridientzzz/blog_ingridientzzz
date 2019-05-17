c0b6338274c9e815a97aa4019b1e61f0913c07b5
ingridientzzz <ingridientzzz@gmail.com>
2019-05-17 22:02:35 +0100

    added user reg & crispy forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# users/views.py
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+# htmls that will get created by Django classes
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, message=f'Account created for {username}.')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', context={'form': form})
