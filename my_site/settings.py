e00c4882f6d95282e13f9742a1246acf2cefba8f
ingridientzzz <ingridientzzz@gmail.com>
2019-05-25 19:38:53 +0100

    DEBUG=False, ALLOWES_HOSTS = []

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jjwn8!h$#gnh0)44wil*^*t(7oqm62j_fs)-d0gu5#s^_qa1ov'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
